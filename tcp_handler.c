d113892b6ebd175963e9e7d1328154038cf24398
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2024-12-29 22:19:23 +0700

    set socket option reuse addr

--- tcp_handler.c
+++ tcp_handler.c
@@ -12,8 +12,18 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int TCP_init_socket() { return socket(AF_INET, SOCK_STREAM, 0); }
+int TCP_init_socket() {
+    int on = 1;
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) return -1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on)) < 0) {
+      close(fd);
+      return -1;
+    }
+    return fd;
+}
 
 int TCP_bind_socket_address(int socketfd, char *port) {
     struct sockaddr_in servaddr;
